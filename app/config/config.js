04c16dbeb8a80dbdcf0ae6cd441b4c092b6aa57e
Иван Табакерка <40301518+IvanTabakerka@users.noreply.github.com>
2024-11-06 21:18:46 +0400

    Конфиги

---
+++ app/config/config.js
@@ -0,0 +1,9 @@
+const config = {
+    port: 3000,
+    query: {
+        // Maximum number of items per request
+        maxItemsPerRequest: 500
+    }
+}
+
+module.exports = config;
---
+++ app/config/db.js
@@ -0,0 +1,17 @@
+module.exports = {
+    HOST: "localhost",
+    USER: "lessons",
+    PASSWORD: "lessons",
+    PORT: 5432,
+    DB: "lessons",
+    dialect: "postgres",
+    operatorsAliases: 0,
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 3000,
+        idle: 10000
+    },
+    logging: console.log,
+    force: false
+};
