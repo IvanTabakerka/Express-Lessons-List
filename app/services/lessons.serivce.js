3060a108a7f6afd29e67fd0eb7fc292eccc7afdf
Иван Табакерка <40301518+IvanTabakerka@users.noreply.github.com>
2024-11-09 05:08:27 +0400

    JSDoc для методов

--- app/services/lessons.serivce.js
+++ app/services/lessons.serivce.js
@@ -41,6 +41,13 @@ exports.lessonsGet = async (date, status, teacherIds, studentsCount, limit, offs
     }
 }
 
+/**
+ * Dynamic params for main query
+ * @param {object} date - an object with the start and end properties (if not specified, will return false to end and start)
+ * @param {object} status - object with properties allow (allow query by parameter) and status (parameter value)
+ * @param {object} studentsCount - Object with start and end parameters (if not specified, will return false to end and start)
+ * @returns {{having: {}, where: {}}}
+ */
 const queryParams = (date, status, studentsCount) => {
     const res = {
         where: {},
@@ -64,17 +71,22 @@ const queryParams = (date, status, studentsCount) => {
     }
     // Lessons by students count
     // Count equals
-    if (studentsCount.start && !studentsCount.end) {
+    if ((studentsCount.start || studentsCount.start === 0) && !studentsCount.end) {
         res.having = db.Sequelize.literal(`${calculateVisitorsSQL} = ${studentsCount.start}`);
     }
     // Count range
-    if (studentsCount.start && studentsCount.end) {
+    if ((studentsCount.start || studentsCount.start === 0) && studentsCount.end) {
         res.having = db.Sequelize.literal(`${calculateVisitorsSQL} BETWEEN ${studentsCount.start} AND ${studentsCount.end}`);
     }
 
     return res;
 }
 
+/**
+ * Includes for main query
+ * @param {array} teacherIds - IDs array [1,2,3,5]
+ * @returns {array}
+ */
 const includesCollector = (teacherIds) => {
     return [
         includeStudents(),
@@ -82,6 +94,11 @@ const includesCollector = (teacherIds) => {
     ];
 }
 
+/**
+ * Teachers join and deep dynamic params
+ * @param {array} teacherIds - IDs array [1,2,3,5]
+ * @returns {object}
+ */
 const includeTeachers = (teacherIds) => {
     let include = {
         model: db.teachers,
@@ -100,6 +117,10 @@ const includeTeachers = (teacherIds) => {
     return include;
 }
 
+/**
+ * Students join
+ * @returns {object}
+ */
 const includeStudents = () => {
     return {
         model: db.students,
@@ -125,44 +146,3 @@ const calculateVisitorsSQL = `(
 FROM "lesson_students"
 WHERE "lesson_students"."lesson_id" = "lessons"."id" AND "lesson_students"."visit" = true
 )::int`;
-
-
-const rawQuery = async () => {
-    const elements = await db.lessons.findAll({
-        attributes: {
-            include: [
-                [db.sequelize.literal(`(
-                            SELECT COUNT(*)
-                            FROM "lesson_students"
-                            WHERE "lesson_students"."lesson_id" = "lessons"."id" AND "lesson_students"."visit" = true
-                        )::int`),
-                    'visitCount']
-            ]
-        },
-        include: [{
-            model: db.students,
-            attributes: {
-                include: [
-                    'id',
-                    'name',
-                    [db.sequelize.literal('"students->lesson_students"."visit"'), 'visit']
-                ]
-            },
-            through: {
-                attributes: []
-            }
-        },{
-            model: db.teachers,
-            through: {
-                attributes: []
-            }
-        }],
-        having: db.Sequelize.literal(`(
-                SELECT COUNT(*)
-                FROM "lesson_students"
-                WHERE "lesson_students"."lesson_id" = "lessons"."id" AND "lesson_students"."visit" = true
-            ) > 0`),
-        group: ['students.id', 'lessons.id', 'teachers.id', 'students->lesson_students.visit']
-    });
-    return elements
-}
